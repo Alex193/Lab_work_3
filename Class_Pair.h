07355fb9e89b759c13197036150140bdfaed9ade
Alex193 <63094098+Alex193@users.noreply.github.com>
2020-11-14 22:02:34 +0300

    Update Class_Pair.h

--- Class_Pair.h
+++ Class_Pair.h
@@ -4,40 +4,40 @@ using namespace std;
 class Pair
 {
 private:
-	int int_num; //îáúÿâèëè öåëî÷èñëåííóþ ïåðåìåííóþ 
-	double double_num; //îáúÿâèëè ïåðåìåííóþ òèïà double
+	int int_num; //объявили целочисленную переменную 
+	double double_num; //объявили переменную типа double
 public:
-	explicit Pair() : int_num(0), double_num(0) {} //êîíñòðóêòîð áåç ïàðàìåòðîâ
-	Pair(const int a, const double b) //êîíñòðóêòîð ñ ïàðàìåòðàìè
+	explicit Pair() : int_num(0), double_num(0) {} //конструктор без параметров
+	Pair(const int a, const double b) //конструктор с параметрами
 	{
 		int_num = a;
 		double_num = b;
 	}
-	friend istream& operator>> (istream& in, Pair& numbers) //ïåðåãðóçêà îïðåðàòîðà ââîäà
+	friend istream& operator>> (istream& in, Pair& numbers) //перегрузка опрератора ввода
 	{
-		cout << "Ââîä ÷èñåë êëàññà Pair" << endl;
-		cout << "Ââåäèòå ÖÅËÎÅ ÷èñëî: ";
-		in >> numbers.int_num; //ñ÷èòûâàåì öåëîå ÷èñëî
-		cout << "Ââåäèòå ÂÅÙÅÑÒÂÅÍÍÎÅ ÷èñëî: ";
-		in >> numbers.double_num; //ñ÷èòûâàåì âåùåñòâåííîå ÷èñëî
+		cout << "Ввод чисел класса Pair" << endl;
+		cout << "Введите ЦЕЛОЕ число: ";
+		in >> numbers.int_num; //считываем целое число
+		cout << "Введите ВЕЩЕСТВЕННОЕ число: ";
+		in >> numbers.double_num; //считываем вещественное число
 		return in;
 	}
-	friend ostream& operator<< (ostream& out, const Pair& numbers) //ïåðåãðóçêà îïðåðàòîðà âûâîäà
+	friend ostream& operator<< (ostream& out, const Pair& numbers) //перегрузка опрератора вывода
 	{
 		out << numbers.int_num << ":" << numbers.double_num;
 		return out;
 	}
-	bool operator== (const Pair& numbers) const //ïåðåãðóçêà îïðåðàòîðà ñðàâíåíèÿ ==
+	bool operator== (const Pair& numbers) const //перегрузка опрератора сравнения ==
 	{
 		return (int_num == numbers.int_num) && (double_num == numbers.double_num);
 	}
-	bool operator!= (const Pair& numbers) const //ïåðåãðóçêà îïðåðàòîðà ñðàâíåíèÿ !=
+	bool operator!= (const Pair& numbers) const //перегрузка опрератора сравнения !=
 	{
 		return !((int_num == numbers.int_num) && (double_num == numbers.double_num));
 	}
-	Pair& operator= (const Pair& numbers) //ïåðåãðóçêà îïåðåàòîðà ïðèñâàèâàíèÿ
+	Pair& operator= (const Pair& numbers) //перегрузка опереатора присваивания
 	{
-		if (this == &numbers) //åñëè ýëåìåíòû ðàâíû, âîçâðàùàåì òåêóùèé
+		if (this == &numbers) //если элементы равны, возвращаем текущий
 		{
 			return *this;
 		}
@@ -45,5 +45,5 @@ public:
 		double_num = numbers.double_num;
 		return *this;
 	}
-	~Pair() { }
+	~Pair() {}
 };
